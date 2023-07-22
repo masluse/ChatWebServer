268205dbd9b8dd68e2c076faee4de9dc16a81b97
masluse <manuel@familie-regli.ch>
2023-07-23 00:00:23 +0200

    Manuel und Quentin haben sich versöhnt

--- Models/Message.cs
+++ Models/Message.cs
@@ -2,11 +2,13 @@
 {
     public class Message
     {
+        [Column("messageID")]
         public int MessageID { get; set; }
+        [Column("value")]
         public string Value { get; set; }
+        [Column("timestamp")]
         public DateTime Timestamp { get; set; }
-
+        [Column("FK_userID")]
         public int FK_userID { get; set; }
-        public User User { get; set; }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -2,8 +2,11 @@
 {
     public class User
     {
+        [Column("userID")]
         public int UserID { get; set; }
+        [Column("username")]
         public string Username { get; set; }
+        [Column("password")]
         public string Password { get; set; }
     }
 }
