1ef14f128acf46937ae3c2c19080a154ab739076
QuQuq <schoenenbergerq@bzz.ch>
2023-07-29 17:14:31 +0200

    fix(db): correct quotes

--- ChatWebServer/DBContext/ApplicationDbContext.cs
+++ ChatWebServer/DBContext/ApplicationDbContext.cs
@@ -63,29 +63,29 @@ namespace ChatWebServer.DBContext
         public void InitializeDatabase()
         {
             string sqlScript = @"
-                CREATE TABLE IF NOT EXISTS Users (
-                    userID SERIAL PRIMARY KEY,
-                    username VARCHAR(55) NOT NULL,
-                    password VARCHAR(999) NOT NULL,
-                    role VARCHAR(55) NOT NULL, 
-                    isActive BOOLEAN NOT NULL
-                );
+            CREATE TABLE IF NOT EXISTS ""Users"" (
+                ""userID"" SERIAL PRIMARY KEY,
+                ""username"" VARCHAR(55) NOT NULL,
+                ""password"" VARCHAR(999) NOT NULL,
+                ""role"" VARCHAR(55) NOT NULL, 
+                ""isActive"" BOOLEAN NOT NULL
+            );
 
-                CREATE TABLE IF NOT EXISTS Messages (
-                    messageID SERIAL PRIMARY KEY,
-                    value VARCHAR(999) NOT NULL,
-                    FK_userID INT NOT NULL,
-                    timeStamp TIMESTAMP NOT NULL
-                );
+            CREATE TABLE IF NOT EXISTS ""Messages"" (
+                ""messageID"" SERIAL PRIMARY KEY,
+                ""value"" VARCHAR(999) NOT NULL,
+                ""FK_userID"" INT NOT NULL,
+                ""timeStamp"" TIMESTAMP NOT NULL
+            );
 
-                -- Add foreign key constraint
-                ALTER TABLE Messages
-                ADD CONSTRAINT fk_user
-                FOREIGN KEY (FK_userID) REFERENCES Users(userID);
+            -- Add foreign key constraint
+            ALTER TABLE ""Messages""
+            ADD CONSTRAINT ""fk_user""
+            FOREIGN KEY (""FK_userID"") REFERENCES ""Users""(""userID"");
 
-                INSERT INTO Users (userID, username, password, role, isActive) VALUES (1, 'Admin', '8C6976E5B5410415BDE908BD4DEE15DFB167A9C873FC4BB8A81F6F2AB448A918', 'ADMIN', true);
-                INSERT INTO Users (userID, username, password, role, isActive) VALUES (2, 'User', '04F8996DA763B7A969B1028EE3007569EAF3A635486DDAB211D512C85B9DF8FB', 'USER', true);
-            ";
+            INSERT INTO ""Users"" (""userID"", ""username"", ""password"", ""role"", ""isActive"") VALUES (1, 'Admin', '8C6976E5B5410415BDE908BD4DEE15DFB167A9C873FC4BB8A81F6F2AB448A918', 'ADMIN', true);
+            INSERT INTO ""Users"" (""userID"", ""username"", ""password"", ""role"", ""isActive"") VALUES (2, 'User', '04F8996DA763B7A969B1028EE3007569EAF3A635486DDAB211D512C85B9DF8FB', 'USER', true);
+        ";
 
             using (var connection = new NpgsqlConnection(GetConnectionString()))
             {
@@ -97,5 +97,6 @@ namespace ChatWebServer.DBContext
                 }
             }
         }
+
     }
 }
