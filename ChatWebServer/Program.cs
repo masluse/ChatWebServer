875b4b344d7379127329218bdf783fa379f7ef17
QuQuq <schoenenbergerq@bzz.ch>
2023-07-30 17:23:24 +0200

    fix(config)

--- ChatWebServer/Program.cs
+++ ChatWebServer/Program.cs
@@ -25,14 +25,6 @@ builder.Services.AddAuthorization(options =>
     options.AddPolicy("AdminOnly", policy => policy.RequireRole("ADMIN"));
 });
 
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin", builder =>
@@ -42,6 +34,14 @@ builder.Services.AddCors(options =>
                .AllowAnyMethod();
     });
 });
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
