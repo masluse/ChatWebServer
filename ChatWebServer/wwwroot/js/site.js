c7460a61494a308a2dc9c71e0a5dd1a0407f62f7
QuQuq <schoenenbergerq@bzz.ch>
2023-08-01 20:32:20 +0200

    fix(add class)

--- ChatWebServer/wwwroot/js/site.js
+++ ChatWebServer/wwwroot/js/site.js
@@ -210,6 +210,7 @@ openNewChatDialogBtn.addEventListener("click", () => {
 
                 const input = document.createElement("input");
                 input.type = "checkbox";
+                input.classList.add("user-checkbox");
                 input.id = `userCheckbox_${user.userID}`;
                 input.value = user.username;
 
@@ -236,24 +237,16 @@ function closeCreateNewChatDialog() {
 }
 
 function createNewChat() {
-    // Get all the user checkboxes by their class name
     const checkboxes = document.getElementsByClassName("user-checkbox");
 
-    // Create an array to store the usernames of checked users
     const checkedUsernames = [];
 
-    // Loop through all the checkboxes
     for (let i = 0; i < checkboxes.length; i++) {
-        // Check if the checkbox is checked
         if (checkboxes[i].checked) {
-            // Get the username from the data attribute of the checkbox
             const username = checkboxes[i].getAttribute("data-username");
             checkedUsernames.push(username);
         }
     }
 
-    // Now, the checkedUsernames array contains the usernames of the checked users
     console.log("Checked Users:", checkedUsernames);
-
-    // Add your logic here to create the chat or perform any other action with the checked users
 }
