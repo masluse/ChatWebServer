0dc0123ef2a576d0b64046af908b9745447af633
QuQuq <schoenenbergerq@bzz.ch>
2023-08-01 16:55:23 +0200

    fix(page reload)

--- ChatWebServer/Controllers/HomeController.cs
+++ ChatWebServer/Controllers/HomeController.cs
@@ -2,13 +2,8 @@
 using ChatWebServer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
