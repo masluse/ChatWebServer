2d00db56b393b11a2c474eb73ece925b34d504df
QuQuq <schoenenbergerq@bzz.ch>
2023-08-01 11:35:10 +0200

    feat(sidenav): add sidenav menu

--- ChatWebServer/wwwroot/js/site.js
+++ ChatWebServer/wwwroot/js/site.js
@@ -170,3 +170,15 @@
 });
 
 window.scrollTo(0, document.body.scrollHeight);
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
